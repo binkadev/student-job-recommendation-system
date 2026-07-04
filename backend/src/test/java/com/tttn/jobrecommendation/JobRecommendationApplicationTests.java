1d1d9cd59a7fc65a6ea406ed48fa4c6034b09cfc
Trần Hoàng Hải ( Bin Dev ) <haihoang1312004@gmail.com>
2026-07-04 23:04:43 +0700

    update based

---
+++ backend/src/test/java/com/tttn/jobrecommendation/JobRecommendationApplicationTests.java
@@ -0,0 +1,12 @@
+package com.tttn.jobrecommendation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobRecommendationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
